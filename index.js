c53471fb560418b3bb6e452699b2b04a4d4e418f
Nabin Islam <sinabin62246@gmail.com>
2022-10-26 01:07:05 +0600

    courses category api done

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const courses = require('./data/courses.json');
+const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
   res.send('LearnHub api is running');
+});
+
+app.get('/courses-categories', (req, res) => {
+  res.send(categories);
+});
+
+app.get('/courses-category/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedCategory = categories.find(category => category.id === id);
+  res.send(selectedCategory);
 });
 
 app.get('/courses', (req, res) => {
@@ -20,6 +31,14 @@ app.get('/course/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const selectedCourse = courses.find(course => course.id === id);
   res.send(selectedCourse);
+});
+
+app.get('/courses/category/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedCoursesCategory = courses.filter(
+    course => course.category_id === id
+  );
+  res.send(selectedCoursesCategory);
 });
 
 app.listen(port, () => {
